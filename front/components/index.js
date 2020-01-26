08efae3e3ae9beeff080de8749d2bc4692589e69
Shugaev Aleksey <alek-2020@yandex-team.ru>
2020-01-26 20:55:55 +0300

    build: routing

--- front/components/index.js
+++ front/components/index.js
@@ -5,12 +5,17 @@ import { configure } from 'mobx';
 import stores from 'stores';
 import Body from 'c/Body';
 import './index.sass';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
 configure({ enforceActions: 'observed' });
 
 ReactDOM.render(
-  <Provider {...stores}>
-    <Body />
-  </Provider>,
+  <Router>
+    <Provider {...stores}>
+      <Body />
+    </Provider>
+  </Router>,
   document.getElementById('app'),
 );
