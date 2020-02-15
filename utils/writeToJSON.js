7fbe9cf72b4a324a08e572152ab807e287194227
Shugaev Aleksey <alek-2020@yandex-team.ru>
2020-02-15 16:45:45 +0300

    feat: хелпер для записи в json

---
+++ utils/writeToJSON.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+/**
+ * Пишет данные в JSON
+ *
+ * @param data
+ * @param name
+ * @killAfter - убивает процесс после записи
+ */
+function writeToJSON({ data, name, killAfter }) {
+  fs.writeFile(
+    name || './data.json',
+    JSON.stringify({ data }), 'utf8', () => (
+      console.log('файл записан'),
+      killAfter && process.exit()
+    ),
+  );
+}
+
+module.exports = writeToJSON;
