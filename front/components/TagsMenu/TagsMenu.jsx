c64c14fcb9bec466476cc175df1095691b28b377
Shugaev Aleksey <alek-2020@yandex-team.ru>
2020-02-24 17:19:01 +0300

    feat: Поправил анимацию в меню

--- front/components/TagsMenu/TagsMenu.jsx
+++ front/components/TagsMenu/TagsMenu.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
 import { inject, observer } from 'mobx-react';
 import Button from 'c/Button';
-import Icon from 'c/Icon';
 import { withRouter } from 'react-router';
 import './TagsMenu.sass';
 
@@ -30,20 +29,19 @@ const TagsMenu = inject('tagsStore', 'tracksStore')(observer(({ tagsStore, track
   return (
     <div className={cnTagsMenu({ isOpened })}>
       <div className={cnTagsMenu('Background')} onClick={toggleIsOpened} />
-      <div className={cnTagsMenu('Sideblock')}>
-        {allTags.map((tag, i) => (
-          <Button
-            onClickArgs={tag._id}
-            onClick={onTagClick}
-            key={i}
-            theme={filterTags.includes(tag._id) ? 'activeLabel' : 'label'}
-            text={tag.name}
-          />
-        ))}
-
-        <div className={cnTagsMenu('ToggleButton')} onClick={toggleIsOpened}>
-          <Icon icon="hash" className={cnTagsMenu('Hash')} />
+      <div className={cnTagsMenu('SideblockWrapper')}>
+        <div className={cnTagsMenu('Sideblock')}>
+          {allTags.map((tag, i) => (
+            <Button
+              onClickArgs={tag._id}
+              onClick={onTagClick}
+              key={i}
+              theme={filterTags.includes(tag._id) ? 'activeLabel' : 'label'}
+              text={tag.name}
+            />
+          ))}
         </div>
+        <Button icon="hash" className={cnTagsMenu('ToggleButton')} onClick={toggleIsOpened} />
       </div>
     </div>
   );
