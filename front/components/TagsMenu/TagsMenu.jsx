0d4f800ae7d702fb0d299381b3a5ff344e31e28a
Shugaev Aleksey <alek-2020@yandex-team.ru>
2020-03-09 16:59:01 +0300

    feat: универсальная кнопка тега в боковом меню

--- front/components/TagsMenu/TagsMenu.jsx
+++ front/components/TagsMenu/TagsMenu.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { cn } from '@bem-react/classname';
 import { inject, observer } from 'mobx-react';
 import Button from 'c/Button';
+import TagButton from 'c/TagButton';
 import { withRouter } from 'react-router';
 import './TagsMenu.sass';
 
 const cnTagsMenu = cn('TagsMenu');
 
-const TagsMenu = inject('tagsStore', 'tracksStore')(observer(({ tagsStore, tracksStore, history }) => {
+const TagsMenu = inject('tagsStore', 'tracksStore')(observer(({ tagsStore }) => {
   const [isOpened, setIsOpened] = useState(false);
 
   const { fetchTagsByIds, pushToAllTags, allTags } = tagsStore;
-  const { setFilterTags, filterTags } = tracksStore;
 
   useEffect(() => {
     fetchTagsByIds(null, pushToAllTags);
@@ -21,23 +21,17 @@ const TagsMenu = inject('tagsStore', 'tracksStore')(observer(({ tagsStore, track
     setIsOpened(!isOpened);
   }
 
-  function onTagClick(ids) {
-    setFilterTags(ids, history);
-    toggleIsOpened();
-  }
-
   return (
     <div className={cnTagsMenu({ isOpened })}>
       <div className={cnTagsMenu('Background')} onClick={toggleIsOpened} />
       <div className={cnTagsMenu('SideblockWrapper')}>
         <div className={cnTagsMenu('Sideblock')}>
           {allTags.map((tag, i) => (
-            <Button
-              onClickArgs={tag._id}
-              onClick={onTagClick}
+            <TagButton
+              onClick={toggleIsOpened}
               key={i}
-              theme={filterTags.includes(tag._id) ? 'activeLabel' : 'label'}
               text={tag.name}
+              id={tag._id}
             />
           ))}
         </div>
