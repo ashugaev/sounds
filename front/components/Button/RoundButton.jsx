ed1e05ff6c67c58d75442ed67a07e2c0d0d38357
Shugaev Aleksey <alek-2020@yandex-team.ru>
2020-03-22 18:10:26 +0300

    fix: фикс неработающего disable состояния кнопки

--- front/components/Button/RoundButton.jsx
+++ front/components/Button/RoundButton.jsx
@@ -3,6 +3,7 @@ import Icon from 'c/Icon';
 import Text from 'c/Text';
 import './RoundButton.sass';
 import { cn } from '@bem-react/classname';
+import j from 'join';
 
 const RoundButton = ({
   icon, onClick, size, className, isHidden, theme, text, onClickArgs, disabled, iconClassName, textColor,
@@ -20,7 +21,7 @@ const RoundButton = ({
 
   return (
     !isHidden && (
-    <div className={`${buttonClassName} ${className || ''}`} onClick={onButtonClick}>
+    <div className={j(buttonClassName, className)} onClick={onButtonClick}>
       {icon && <Icon icon={icon} size={size} className={iconClassName} />}
       {text && <Text text={text} size={size} color={textColor} className={cnButton('Text')} />}
     </div>
